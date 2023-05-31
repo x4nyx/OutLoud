a39f931b424f2c39fe9e53e02028a762e1a91fdf
Sivakov Alex <aleksei.s.sivakov@gmail.com>
2023-06-01 00:16:59 +0300

    add action

---
+++ src/main/java/fpmibsu/outloud/action/SearchAction.java
@@ -0,0 +1,16 @@
+package fpmibsu.outloud.action;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SearchAction extends Act{
+    @Override
+    public void updatedata(HttpServletRequest req) {
+
+    }
+
+    @Override
+    public void doAction(HttpServletRequest req, HttpServletResponse res) {
+
+    }
+}
